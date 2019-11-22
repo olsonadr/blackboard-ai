224adfb09ebac570948d0ab64c0669dbb054a5b7
mrbluesky2 <pierrecarr2@gmail.com>
2019-11-21 17:27:09 -0800

    added a clear button

--- public/canvas.js
+++ public/canvas.js
@@ -20,6 +20,20 @@ window.addEventListener("load", () => {
     canvas.addEventListener("mousemove", draw);
     canvas.addEventListener("mouseup", endDraw);
 
+    const clear = document.querySelector("#clear-button");
+
+    clear.onclick = function () {
+        // Store the current transformation matrix
+        ctx.save();
+
+        // Use the identity matrix while clearing the canvas
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Restore the transform
+        ctx.restore();
+    };
+
     function startDraw (e) {
         drawing = true;
         draw(e);
